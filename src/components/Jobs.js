5f001392de555a17b73dc006673ef3e7afc49be3
Michael Stallings <stall84@gmail.com>
2020-06-20 13:07:16 -0400

    initial commit

---
+++ src/components/Jobs.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import './jobs.css';
+import JobsListItem from './JobsListItem';
+import axios from 'axios';
+
+class Jobs extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            jobs: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get('/api/jobs')
+            .then((response) => {
+                this.setState({
+                    jobs: response.data
+                })
+            })
+    }
+
+    render() {
+        const jobsJSX = this.state.jobs.map((job, index) => {
+            // Because the axios call's payload is a JSON formatted object, we can just simply
+            // spread in each job iteration element, and it will know to assign the key/value's for each
+            // You also have to assign a key prop set to the index of the array being iterated over
+            // Pass this down in props to the JobsListItem component
+            return <JobsListItem key={index} {...job} />
+        })
+        return (
+            <div>
+                <div className="App">
+                    <header className="App-header">
+                        <h1 className="App-title">Jobs in Atlanta</h1>
+                        <p className="App-subtitle">Click to explore jobs</p>
+                    </header>
+                </div>
+                <div className="Jobs">
+                  {jobsJSX}
+                </div>
+            </div>
+        );
+    }
+
+}
+
+
+export default Jobs;
---
+++ src/components/JobsListItem.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './JobsListItem.css';
+
+
+class JobsListItem extends Component {
+
+    render() {
+        return (
+            <div className="JobsListItem">
+                <div className="left">
+                    <b>{this.props.title}</b>
+                    <div>{this.props.company}</div>
+                </div>
+                <div className="Right">
+                    <b>{this.props.type}</b>
+                    <div>{this.props.created_at}</div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default JobsListItem;
